48d513f69938715d31b06e14d589ed640f7dd168
MerlinMinder <merlin.coding@gmail.com>
2022-10-27 10:29:52 +0200

    added player

--- bot.py
+++ bot.py
@@ -7,6 +7,23 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 
 
+class Player(object):
+    def __init__(self, user, message) -> None:
+        self.level = 0
+        self.name = user
+        self.message = message
+
+    async def hello(self, message):
+        await self.message.channel.send(f"Hello {self.name}")
+
+    async def levelup(self, increase):
+        self.level += increase
+        await self.message.channel.send(f"Your level is {self.level}")
+
+
+PLAYERS = {}
+
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
@@ -22,8 +39,30 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send(f'Hello, {message.author.name}')
 
+    if message.content.startswith("$start"):
+        PLAYERS[message.author.name] = Player(message.author.name, message)
+        await PLAYERS[message.author.name].hello()
+
+    if message.content.startswith("$levelup"):
+        await PLAYERS[message.author.name].levelup(int(message.content.split()[1]))
+
     if message.content.startswith("$multiply"):
-        await message.channel.send(int(message.content.split()[1])*int(message.content.split()[2]))
+        numbers = message.content.split()[1]
+        await message.channel.send(int(numbers.split("*")[0])*int(numbers.split("*")[1]))
+
+    if message.content.startswith("$calculate"):
+        calculate = message.content.split()[1]
+        if calculate[1] == "-":
+            await message.channel.send(int(calculate[0])-int(calculate[2]))
+
+        if calculate[1] == "+":
+            await message.channel.send(int(calculate[0])+int(calculate[2]))
+
+        if calculate[1] == "/":
+            await message.channel.send(int(calculate[0])/int(calculate[2]))
+
+        if calculate[1] == "*":
+            await message.channel.send(int(calculate[0])*int(calculate[2]))
 
 
 client.run(token)
--- testing.py
+++ testing.py
@@ -1,3 +1,20 @@
-message = "$multiply 4*5"
+bl = "$multiply 4*5"
 
-print(message.split())
+numbers = bl.split()[1]
+print(int(numbers.split("*")[0])*int(numbers.split("*")[1]))
+
+message = "$calculate 3*6"
+
+calculate = message.split()[1]
+
+if calculate[1] == "-":
+    print(int(calculate[0])-int(calculate[2]))
+
+if calculate[1] == "+":
+    print(int(calculate[0])+int(calculate[2]))
+
+if calculate[1] == "/":
+    print(int(calculate[0])/int(calculate[2]))
+
+if calculate[1] == "*":
+    print(int(calculate[0])*int(calculate[2]))
