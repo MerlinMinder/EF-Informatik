a0d6f2de0607947e0d8ab20381c254ebd76abe55
MerlinMinder <merlin.coding@gmail.com>
2022-10-28 13:21:58 +0200

    random testing

--- bot.py
+++ bot.py
@@ -11,14 +11,22 @@ class Player(object):
     def __init__(self, user, message) -> None:
         self.level = 0
         self.name = user
-        self.message = message
+        self.stats = {"health": 100, "damage": 10}
+        self.channel = message.channel
 
-    async def hello(self, message):
-        await self.message.channel.send(f"Hello {self.name}")
+    async def hello(self):
+        await self.channel.send(f"Hello {self.name}")
 
     async def levelup(self, increase):
         self.level += increase
-        await self.message.channel.send(f"Your level is {self.level}")
+        await self.channel.send(f"Your level is {self.level}")
+
+
+class Enemy(object):
+    def __init__(self) -> None:
+        self.stats = {"health": 100, "damage": 10}
+        self.type = "dragon"
+        self.pre_type = "grand"
 
 
 PLAYERS = {}
